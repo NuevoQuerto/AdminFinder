40e85f8894d36514fddb349daaab322f69057093
NuevoQuerto <NuevoQuerto@gmail.com>
2017-06-04 11:59:03 +0700

    Update AdminFinder.py

--- AdminFinder.py
+++ AdminFinder.py
@@ -19,12 +19,15 @@ Parser.add_argument("-u", "--url", help="Target URL, Example: -u www.kpu.go.id",
 Args = Parser.parse_args()
 
 Admin_Page = [
-				"admin.php", "admin", "admin/index.php", "admin/admin.php", "admin/login.php",
-				"login.php", "login", "login/index.php", "login/admin.php", "login/login.php",
-				"wp-login.php", "wp-admin.php", "wp-admin"
-			] # Halaman Login Admin
+		"admin.php", "admin", "admin/index.php", "admin/admin.php", "admin/login.php", "admin/administrator.php", "admin/dsp_login.php",
+		"login.php", "login", "login/index.php", "login/admin.php", "login/login.php", "login/administrator.php", "login/dsp_login.php",
+		"administrator.php", "administrator", "administrator/index.php", "administrator/admin.php", "administrator/login.php", "administrator/administrator.php", "administrator/dsp_login.php",
+		"admin_mato.php", "admin_mato", "admin_mato/index.php", "admin_mato/admin.php", "admin_mato/login.php", "admin_mato/administrator.php", "admin_mato/dsp_login.php",
+		"dashboard.php", "dashboard", "dashboard/index.php", "dashboard/admin.php", "dashboard/login.php", "dashboard/administrator.php", "dashboard/dashboard.php", "dashboard/dashboard.php",
+		"wp-login.php", "wp-admin.php", "wp-admin"
+	] # Halaman Login Admin
 			
-HTTP_Code = "200 OK|301 Moved Permanently|302 Found|302 Moved Temporarily" # HTTP Response Code
+HTTP_Code = "200|301|302" # HTTP Response Code
 
 try:
 	if Args.URL:
